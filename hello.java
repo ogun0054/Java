703ea8aff25459d0ac448b44ed3249fc30bcd716
github_i-slot <ismailadesholaharun@gmail.com>
2023-11-28 04:47:58 +0100

    initial commit

---
+++ hello.java
@@ -0,0 +1,25 @@
+class Hello 
+{
+    public static void main(String a[])
+    {
+        // float score = 6.6f;
+        // byte by = 123;
+
+        // System.out.println(3 + 5);
+        // System.out.println(marks);
+
+        int num1 = 9;
+        byte by = 124;
+        short sh = 556;
+        long l = 68456l;
+
+        float f = 5.8f;
+        double d = 5.8;
+        char c = 'k';
+        boolean b = true;
+
+        
+    }
+}
+
+ 
