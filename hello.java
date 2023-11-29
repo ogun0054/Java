955a253311d948be2b76aa05716266b050f018a5
github_i-slot <ismailadesholaharun@gmail.com>
2023-11-29 03:43:36 +0100

    type conversion and casting

--- hello.java
+++ hello.java
@@ -1,22 +1,33 @@
 class Hello 
 {
-    public static void main(String a[])
+    public static void main(String args[])
     {
+
+        //byte b = 127;
+        int a = 12;
+        byte k = (byte) a;
+
+
+        // System.out.println(b);
+        System.out.println(a);
         // float score = 6.6f;
         // byte by = 123;
 
         // System.out.println(3 + 5);
         // System.out.println(marks);
+    // literals
+        // int num1 = 0b101;
+        // System.out.println(num1);
+        // int num2 = 0x7E;
+        // System.out.println(num2);
+        // byte by = 124;
+        // short sh = 556;
+        // long l = 68456l;
 
-        int num1 = 9;
-        byte by = 124;
-        short sh = 556;
-        long l = 68456l;
-
-        float f = 5.8f;
-        double d = 5.8;
-        char c = 'k';
-        boolean b = true;
+        // float f = 5.8f;
+        // double d = 5.8;
+        // char c = 'k'; // literals
+        // boolean b = true;
 
         
     }
