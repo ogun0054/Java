a986b9b72871d7090d0fb93ff575d866b7d0fb76
github_i-slot <ismailadesholaharun@gmail.com>
2024-01-01 20:01:12 +0100

    Pattern Programming

---
+++ Loop-And-Pattern-Programming/index.java
@@ -0,0 +1,13 @@
+class Loop{
+    public static void main(String[] args)
+    {
+    //   System.out.println("Working.......");
+    
+    // for loop
+
+    for(int i = 0; i < 10; i++)
+    {
+        System.out.println("*");
+    }
+    }
+}
---
+++ pattern-programming/pattern.java
@@ -0,0 +1,17 @@
+class Pattern
+{
+    public static void main(String[] args)
+    {
+      // System.out.println("Done");
+
+      int n = 11;
+      for(int i = 0; i < n; j++)
+      {
+        for(int j = 0; j < n; j++)
+        {
+          if(i==0 && j > 0 && j < 3*(n-1)/4 || j==0 i > 0 && i < n-1 ||
+                )
+        }
+      }
+    }
+}
