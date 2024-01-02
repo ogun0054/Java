52334ae5d5cec7e6f7a5e1215f7adc07f7c8c33d
github_i-slot <ismailadesholaharun@gmail.com>
2024-01-02 01:10:03 +0100

    Square pattern done

--- Loop-And-Pattern-Programming/index.java
+++ Loop-And-Pattern-Programming/index.java
@@ -7,7 +7,23 @@ class Loop{
 
     for(int i = 0; i < 10; i++)
     {
-        System.out.println("*");
+        //System.out.println("*");
     }
+
+    // While loop
+    int j = 0;
+    while(j < 5)
+    {
+        // System.out.println("*");
+        j++;
+    }
+
+    // do while loop
+    int k = 10;
+    do 
+    {
+        System.out.println("*");
+        k++;
+    } while(k < 5);
     }
 }
--- Loop-And-Pattern-Programming/pattern.java
+++ Loop-And-Pattern-Programming/pattern.java
@@ -5,7 +5,35 @@ class Pattern
 
         for(int i = 0; i < 5; i++)
         {
-        System.out.print("*");
+        //System.out.print("*");
+        }
+
+        //Nested loop
+        // int n = 5;
+        // for(int i = 0; i < n; i++)
+        // {
+        //   for(int j = 0; j < n; j++)
+        //    {
+        //       System.out.print("*");
+        //    }
+        // System.out.println(" *" + i);
+        // }
+
+        // Square shape
+        int n = 5;
+        for(int i = 0; i < n; i++)
+        {
+            for(int j = 0; j < n; j++)
+            {
+                if(i == 0 || i ==  n -1 || j == 0 || j == n - 1){
+                System.out.print("*");
+                }
+                else{
+                System.out.print(" ");
+                }
+
+            }
+            System.out.println();
         }
     }
 }
